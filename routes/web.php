04bc5ea3e8954db6fd8a9c9b91ad44c072f2a7ce
dp~ <457509824@qq.com>
2018-06-20 10:17:36 +0800

    邮箱验证中间件

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,23 @@
 |
 */
 
+// 首页
 Route::get('/', 'PagesController@root')->name('root');
+
+// 权限路由
+Auth::routes();
+
+// 需要登录的路由
+Route::group(['middleware' => 'auth'], function () {
+    // 邮箱验证
+    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+
+    // 邮箱验证通过的路由
+    Route::group(['middleware' => 'email_verified'], function () {
+
+    });
+});
 
 Route::get('test', function () {
 
 })->name('app.test');
-
-Auth::routes();
