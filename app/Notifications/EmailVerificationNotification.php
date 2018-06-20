06582c3d375befb449637b4f6d081cee0d966560
dp~ <457509824@qq.com>
2018-06-20 15:23:47 +0800

    触发激活邮件

---
+++ app/Notifications/EmailVerificationNotification.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class EmailVerificationNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable)
+    {
+        // 生成随机字符串
+        $token = str_random(16);
+        // 存入缓存 1800s
+        cache(['email_verification_' . $notifiable->email => $token], 1800);
+        // 邮件按钮跳转地址
+        $url = route('email_verify_notice.verify', ['email' => $notifiable->email, 'token' => $token]);
+
+        return (new MailMessage)
+            ->greeting($notifiable->name . '您好：')
+            ->subject('注册成功，请验证您的邮箱')
+            ->line('请点击下方链接验证您的邮箱')
+            ->action('验证', $url);
+
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,12 @@ Auth::routes();
 
 // 需要登录的路由
 Route::group(['middleware' => 'auth'], function () {
-    // 邮箱验证
+    // 发送验证邮件
+    Route::get('/email_verification/send','EmailVerificationController@send')->name('email_verification.send');
+    // 邮箱验证提示
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
-
+    // 邮箱验证
+    Route::get('/email_verify_notice/verify','EmailVerificationController@verify')->name('email_verify_notice.verify');
     // 邮箱验证通过的路由
     Route::group(['middleware' => 'email_verified'], function () {
 
