382b8cf3cece3d4d1ef599d67f7e52d22a37f42e
dp~ <457509824@qq.com>
2018-09-25 09:33:51 +0800

    订单列表页

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,8 @@ Route::group(['middleware' => 'auth'], function () {
         Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
         // 下单
         Route::post('orders', 'OrdersController@store')->name('orders.store');
+        // 订单列表
+        Route::get('orders','OrdersController@index')->name('orders.index');
     });
 });
 
